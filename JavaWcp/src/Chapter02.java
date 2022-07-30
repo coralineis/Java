e7dda59ee846bbe173cdb2419a64262ba871a55e
coralineis <100649638+coralineis@users.noreply.github.com>
2022-07-30 11:26:17 +0900

    chap3

--- JavaWcp/src/Chapter02.java
+++ JavaWcp/src/Chapter02.java
@@ -1,6 +1,7 @@
 
 public class Chapter02 {
 	public static void main(String[] args) {
-		System.out.println("こんにちは");
+		System.out.println("Hello WEBCAMP!");
+		System.out.println("Hello Java!");
 	}
 }
