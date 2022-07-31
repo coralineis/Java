8e6d2377f1d37850f51cf75cf295770da2bbfec9
coralineis <100649638+coralineis@users.noreply.github.com>
2022-07-31 21:55:31 +0900

    chap7 if else

--- JavaWcp/src/If01.java
+++ JavaWcp/src/If01.java
@@ -4,12 +4,12 @@ public class If01 {
 		int number = 7;
 		if (number < 5) {
 			System.out.println(number + "は5未満です");
-		}
-		if (number < 10) {
-			System.out.println(number + "は10未満です");
-		}
-		if (number < 15) {
-			System.out.println(number + "は15未満です");
+		} else if (number < 10) {
+			System.out.println(number + "は5以上、10未満です");
+		} else if (number < 15) {
+			System.out.println(number + "は10以上、15未満です");
+		} else {
+			System.out.println(number + "は15未満ではないです");
 		}
 	}
 }
