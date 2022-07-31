1f94d2b299cc960af99d44bd338d70c705adbf87
coralineis <100649638+coralineis@users.noreply.github.com>
2022-07-31 21:31:45 +0900

    chap5 exercise

---
+++ JavaWcp/src/Chapter05.java
@@ -0,0 +1,25 @@
+
+public class Chapter05 {
+	public static void main(String[] args) {		long long1;
+		long l = 33333333333L;
+		System.out.println(l);
+
+		double d = 3.14;
+		System.out.println(d);
+
+		boolean b = true;
+		System.out.println(b);
+
+		Float f = 1.1f;
+		System.out.println(f);
+
+		Integer i = 900;
+		System.out.println(i);
+
+		Character c = 'あ';
+		System.out.println(c);
+
+		int[] array = { 2, 78, 82, 828, 87997 };
+		System.out.println(array[3]);
+	}
+}
