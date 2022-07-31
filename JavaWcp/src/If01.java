a37a86830c07b6a6920a6828be1700a1ec1b8eab
coralineis <100649638+coralineis@users.noreply.github.com>
2022-07-31 21:52:16 +0900

    chap7

---
+++ JavaWcp/src/If01.java
@@ -0,0 +1,15 @@
+
+public class If01 {
+	public static void main(String[] args) {
+		int number = 7;
+		if (number < 5) {
+			System.out.println(number + "は5未満です");
+		}
+		if (number < 10) {
+			System.out.println(number + "は10未満です");
+		}
+		if (number < 15) {
+			System.out.println(number + "は15未満です");
+		}
+	}
+}
