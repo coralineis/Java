c4e4d29bf7e8dd24a60cfa2f747d3a3a71d76f20
coralineis <100649638+coralineis@users.noreply.github.com>
2022-08-01 19:56:50 +0900

    chap8 for2

---
+++ JavaWcp/src/For02.java
@@ -0,0 +1,9 @@
+
+public class For02 {
+	public static void main(String[] args) {
+		int[] array = { 1, 2, 3, 4, 5 };
+		for (int number : array) {
+			System.out.println("For02 = " + number);
+		}
+	}
+}
