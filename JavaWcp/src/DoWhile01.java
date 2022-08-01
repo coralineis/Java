90d70e964e4dd1556b6aab550d8fbd2ad7cb2e88
coralineis <100649638+coralineis@users.noreply.github.com>
2022-08-01 16:28:05 +0900

    chap8 do while

---
+++ JavaWcp/src/DoWhile01.java
@@ -0,0 +1,10 @@
+
+public class DoWhile01 {
+	public static void main(String[] args) {
+		int number = 1;
+		do {
+			number *= 2;
+			System.out.println("DoWhile01 = " + number);
+		} while (number < 50);
+	}
+}
