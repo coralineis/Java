804769410ec7482b0d2eabd5237844b11ca62198
coralineis <100649638+coralineis@users.noreply.github.com>
2022-08-13 21:29:15 +0900

    chap10 exception

---
+++ JavaWcp/src/exeption/Exception01.java
@@ -0,0 +1,10 @@
+package exeption;
+
+public class Exception01 {
+	public static void main(String[] args) {
+		System.out.println("100 ÷ 0 は？");
+		int result = 100 / 0;
+		System.out.println("計算結果" + result);
+		System.out.println("プログラム終了");
+	}
+}
