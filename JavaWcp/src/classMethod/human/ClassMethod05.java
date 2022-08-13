e966cb11dc6d351e4bc496c5fd8745a934a34298
coralineis <100649638+coralineis@users.noreply.github.com>
2022-08-13 20:58:20 +0900

    chap9 private

---
+++ JavaWcp/src/classMethod/human/ClassMethod05.java
@@ -0,0 +1,10 @@
+package classMethod.human;
+
+import classMethod.human.Human05;
+
+public class ClassMethod05 {
+	public static void main(String[] args) {
+		Human05 yamada = new Human05();
+		System.out.println("名前は" + yamada.getName() + "で、年齢は" + yamada.getAge() + "です。");
+	}
+}
---
+++ JavaWcp/src/classMethod/human/Human05.java
@@ -0,0 +1,19 @@
+package classMethod.human;
+
+public class Human05 {
+	private String name;
+	private int age;
+
+	public Human05() {
+		name = "山田";
+		age = 20;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public int getAge() {
+		return age;
+	}
+}
