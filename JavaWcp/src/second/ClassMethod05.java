06f63f6c510b4e631d1c77516ac2f127ce6c10e8
coralineis <100649638+coralineis@users.noreply.github.com>
2022-08-15 16:45:40 +0900

    chap9 human05(2)

---
+++ JavaWcp/src/second/ClassMethod05.java
@@ -0,0 +1,11 @@
+package second;
+
+import second.Human05;
+
+public class ClassMethod05 {
+	public static void main(String[] args) {
+		Human05 yamada = new Human05();
+
+		System.out.println("名前は" + yamada.getName() + "で、年齢は" + yamada.getAge() + "です");
+	}
+}
