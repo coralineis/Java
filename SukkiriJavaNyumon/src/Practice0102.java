512ed8866e534cf6756060f0123b9e6e6d0190f9
coralineis <100649638+coralineis@users.noreply.github.com>
2022-08-16 13:48:13 +0900

    chap1 practice1-2

---
+++ SukkiriJavaNyumon/src/Practice0102.java
@@ -0,0 +1,9 @@
+public class Practice0102 {
+	public static void main(String[] args) {
+		int a = 5;
+		int b = 3;
+		int c = a * b;
+
+		System.out.println("縦幅" + a + "横幅" + b + "の長方形の面積は、" + c);
+	}
+}
