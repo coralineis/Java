db0f90fe28daf17824cd3ac21e8601a6b9b42bf7
coralineis <100649638+coralineis@users.noreply.github.com>
2022-08-16 20:40:23 +0900

    chap3 practice3-6

---
+++ SukkiriJavaNyumon/src/Practice0306.java
@@ -0,0 +1,21 @@
+
+public class Practice0306 {
+	public static void main(String[] args) {
+		System.out.println("数当てゲーム");
+		int ans = new java.util.Random().nextInt(10);
+
+		for (int i = 0; i < 5; i++) {
+			System.out.println("0~9の数字を入力してください");
+			int num = new java.util.Scanner(System.in).nextInt();
+
+			if (num == ans) {
+				System.out.println("アタリ！");
+				break;
+			} else {
+				System.out.println("違います");
+			}
+		}
+
+		System.out.println("ゲームを終了します");
+	}
+}
