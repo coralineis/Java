e54f6c195f412bb9b6abc6e86bcd4622be45e7b5
coralineis <100649638+coralineis@users.noreply.github.com>
2022-08-16 19:48:52 +0900

    chap2 practice2-3

---
+++ SukkiriJavaNyumon/src/Practice0203.java
@@ -0,0 +1,20 @@
+
+public class Practice0203 {
+	public static void main(String[] args) {
+		System.out.println("ようこそ占いの館へ");
+		System.out.println("あなたの名前を入力してください");
+		String name = new java.util.Scanner(System.in).nextLine();
+
+		System.out.println("貴方の年齢を入力してください");
+		String ageString = new java.util.Scanner(System.in).nextLine();
+
+		int age = Integer.parseInt(ageString);
+
+		int fortune = new java.util.Random().nextInt(4);
+		fortune++;
+
+		System.out.println("占いの結果が出ました！");
+		System.out.println(age + "歳の" + name + "さん、あなたの運気番号は" + fortune + "です");
+		System.out.println("1:大吉 2:中吉 3:吉 4:凶");
+	}
+}
