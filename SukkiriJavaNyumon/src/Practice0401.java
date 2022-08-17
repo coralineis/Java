ddb1fc15c8c72f1c1ceb92e1e24e9575b2f9f846
coralineis <100649638+coralineis@users.noreply.github.com>
2022-08-17 10:56:43 +0900

    chap4 practice4-1

---
+++ SukkiriJavaNyumon/src/Practice0401.java
@@ -0,0 +1,9 @@
+
+public class Practice0401 {
+	public static void main(String[] args) {
+		int[] points = new int[4];
+		double[] weights = new double[5];
+		boolean[] answers = new boolean[3];
+		String[] names = new String[3];
+	}
+}
