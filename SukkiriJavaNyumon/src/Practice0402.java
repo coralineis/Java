59448577039d9316108fecf83b76c0ef4ceadf5b
coralineis <100649638+coralineis@users.noreply.github.com>
2022-08-17 11:39:59 +0900

    chap4 practice4-2

---
+++ SukkiriJavaNyumon/src/Practice0402.java
@@ -0,0 +1,13 @@
+
+public class Practice0402 {
+	public static void main(String[] args) {
+		int[] moneyList = {121902, 8302, 55100};
+		for (int i = 0; i < moneyList.length; i++) {
+			System.out.println(moneyList[i]);
+		}
+
+		for (int value : moneyList) {
+			System.out.println(value);
+		}
+	}
+}
