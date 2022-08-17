509de19eb9e0fe79998ff82353ce33f5a69b1870
coralineis <100649638+coralineis@users.noreply.github.com>
2022-08-17 20:14:02 +0900

    chap5 practice5-1

---
+++ SukkiriJavaNyumon/src/Practice0501.java
@@ -0,0 +1,14 @@
+
+public class Practice0501 {
+	public static void main(String[] args) {
+		introduceOneself();
+	}
+
+	public static void introduceOneself() {
+		String name = "tay";
+		int age = 20;
+		double height = 176.5;
+		char zodiac = '丑';
+		System.out.println("名前は" + name + "、年齢は" + age + "、身長は、" + height + "、干支は" + zodiac + "です");
+	}
+}
