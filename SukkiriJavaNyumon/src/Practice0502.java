bfa3f2695f17d057257e799d774ec3344de65869
coralineis <100649638+coralineis@users.noreply.github.com>
2022-08-17 20:31:47 +0900

    chap5 practice5-3

--- SukkiriJavaNyumon/src/Practice0502.java
+++ SukkiriJavaNyumon/src/Practice0502.java
@@ -6,11 +6,19 @@ public class Practice0502 {
 		String text = "カメカメカメカメ";
 
 		email(title, address, text);
+
+		email(address, text);
 	}
 
 	public static void email(String title, String address, String text) {
 		System.out.println(address + "に、以下のメールを送信しました。");
 		System.out.println("件名： " + title);
+		System.out.println("本文： " + text);
+	}
+
+	public static void email(String address, String text) {
+		System.out.println(address + "に、以下のメールを送信しました。");
+		System.out.println("件名： 無題");
 		System.out.println("本文： " + text);
 	}
 }
