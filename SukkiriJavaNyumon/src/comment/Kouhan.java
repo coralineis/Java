01bb48e59a9d2a8cce5c1e8cc9f2e50613b0d834
coralineis <100649638+coralineis@users.noreply.github.com>
2022-08-18 20:16:37 +0900

    chap6 practice6-3

--- SukkiriJavaNyumon/src/comment/Kouhan.java
+++ SukkiriJavaNyumon/src/comment/Kouhan.java
@@ -8,6 +8,7 @@ public class Kouhan {
 	public static void showMondokoro() throws Exception {
 		System.out.println("飛車さん、角さん。もういいでしょう。");
 		System.out.println("この紋所が目に入らぬか！");
+		Thread.sleep(3000);
 		Zenhan.doTogame();
 	}
 }
