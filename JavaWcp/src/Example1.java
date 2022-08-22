6a01d3c19cc9f3a36375df2dcd8a1776366fbb80
coralineis <100649638+coralineis@users.noreply.github.com>
2022-08-22 21:00:35 +0900

    practice1

--- JavaWcp/src/Example1.java
+++ JavaWcp/src/Example1.java
@@ -1,9 +1,17 @@
 
 public class Example1 {
 	public static void main(String[] args) {
-		int x = 3; int y = 8;
-		if (x > y) {
-			System.out.println("xはyより大きい");
+		int sum = 0;
+		int count = 0;
+
+		for (int a = 1; a <= 10; a++) {
+			if (a % 2 == 0) {
+				System.out.println(a);
+				sum = sum + a;
+				count++;
+			}
 		}
+		System.out.println(sum);
+		System.out.println(count);
 	}
 }
