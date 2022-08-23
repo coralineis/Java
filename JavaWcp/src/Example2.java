51e50494116db671bc6ac36eed86a32acd1a342a
coralineis <100649638+coralineis@users.noreply.github.com>
2022-08-23 16:18:15 +0900

    practice3

---
+++ JavaWcp/src/Example2.java
@@ -0,0 +1,11 @@
+
+public class Example2 {
+	public static void main(String[] args) {
+		for (int i = 1; i < 11; i++) {
+			System.out.print(i);
+			if(i < 10) {
+				System.out.print(",");
+			}
+		}
+	}
+}
