833f7b68c5ca09ff8af70350a80cdd86cec02828
coralineis <100649638+coralineis@users.noreply.github.com>
2022-08-25 18:52:51 +0900

    chap13 default

---
+++ JavaWcp/src/extend/Interface02.java
@@ -0,0 +1,10 @@
+package extend;
+
+import extend.phone.MobilePhone;
+
+public class Interface02 {
+	public static void main(String[] args) {
+		MobilePhone mobilePhone = new MobilePhone("000-1111-2222");
+		mobilePhone.powerOff();
+	}
+}
--- JavaWcp/src/extend/phone/Phone.java
+++ JavaWcp/src/extend/phone/Phone.java
@@ -1,7 +1,11 @@
 package extend.phone;
 
 public interface Phone {
-	int MAX_NUMBER_DIGITS = 11;
+	final int MAX_NUMBER_DIGITS = 11;
 
-	public void call(String number);
+	void call(String number);
+
+	default void powerOff() {
+		System.out.println("電源を切ります。");
+	}
 }
