14994cd230f0591af7bcb03232b624eb6a3096bc
coralineis <100649638+coralineis@users.noreply.github.com>
2022-08-28 21:05:09 +0900

    practice2-1

---
+++ JavaWcp/src/Example5.java
@@ -0,0 +1,15 @@
+
+public class Example5 {
+	public static void main(String[] args) {
+		int count = 0;
+		int sum = 0;
+
+		for (int a = 1; a <= 10; a++) {
+			if (a % 2 == 0) {
+				count ++;
+
+			}
+		}
+		System.out.println(count);
+	}
+}
---
+++ JavaWcp/src/Example6.java
@@ -0,0 +1,6 @@
+
+public class Example6 {
+	public static void main(String[] args) {
+		
+	}
+}
