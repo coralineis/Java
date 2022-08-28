19614cf30ef4f27b33f48923cbdd69b2e8e110eb
coralineis <100649638+coralineis@users.noreply.github.com>
2022-08-28 21:09:58 +0900

    practice3

--- JavaWcp/src/Example6.java
+++ JavaWcp/src/Example6.java
@@ -1,6 +1,13 @@
 
 public class Example6 {
 	public static void main(String[] args) {
-		
+		for (int a = 1; a <= 10; a++) {
+			if (a % 2 == 0) {
+				System.out.print(a);
+				if (a < 10) {
+					System.out.print(",");
+				}
+			}
+		}
 	}
 }
