7971b52c9c9d0b98855526a32011f2247e0aa004
coralineis <100649638+coralineis@users.noreply.github.com>
2022-08-29 20:08:03 +0900

    practice1

--- JavaWcp/src/Example7.java
+++ JavaWcp/src/Example7.java
@@ -2,19 +2,19 @@
 public class Example7 {
 	public static void main(String[] args) {
 		int [] a = {2, 5, 6, 7, 9, 13, 15, 16, 18, 22, 23, 25, 29, 30, 33, 37, 39, 40, 43, 44};
-		for (int i = 0; i < 11; i++) {
+		for (int i = 0; i < 10; i++) {
 			if (a[i] % 2 != 0) {
 				System.out.print (a[i]);
-				if (i < 10) {
+				if (i < 6) {
 					System.out.print(",");
 				}
 			}
 		}
 
-		for (int i = 0; i > 10 && i < 21; i++) {
-			if (a[i] % 2 == 0) {
-				System.out.print(a[i]);
-				if (i < 20) {
+		for (int n = 0; n > 10 && n < 21; n++) {
+			if (a[n] % 2 == 0) {
+				System.out.print(a[n]);
+				if (n < 20) {
 					System.out.print(",");
 				}
 			}
---
+++ JavaWcp/src/Example8.java
@@ -0,0 +1,10 @@
+
+public class Example8 {
+	public static void main(String[] args) {
+		for (int a = 1; a <= 10; a++) {
+			if (a % 2 == 0) {
+				System.out.println(a);
+			}
+		}
+	}
+}
