9ffe101bd936f0652f242996b6d869f2ac3e692e
coralineis <100649638+coralineis@users.noreply.github.com>
2022-08-30 16:25:46 +0900

    practice2

--- JavaWcp/src/Example12.java
+++ JavaWcp/src/Example12.java
@@ -1,10 +1,24 @@
 
 public class Example12 {
 	public static void main(String[] args) {
+		System.out.println("1問目");
 		for (int i = 1; i < 11; i++) {
 			if (i % 2 == 0) {
 				System.out.println(i);
 			}
 		}
+
+		System.out.println("2問目");
+		int count = 0;
+		int sum = 0;
+
+		for (int i = 1; i < 11; i++) {
+			if (i % 2 == 0) {
+				count++;
+				sum += i;
+			}
+		}
+		System.out.println("1~10までに偶数は" + count + "コ");
+		System.out.println("1~10までの偶数の合計は" + sum);
 	}
 }
