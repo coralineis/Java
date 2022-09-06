d0f6034c9d8424024868c3a0d859d602eaabd124
coralineis <100649638+coralineis@users.noreply.github.com>
2022-09-06 12:36:34 +0900

    sjn 8-2 hero class

---
+++ SukkiriJavaNyumon/src/game/Hero.java
@@ -0,0 +1,26 @@
+package game;
+
+public class Hero {
+	String name;
+	int hp;
+
+	public void attack() {
+
+	}
+
+	public void sleep() {
+
+	}
+
+	public void sit(int sec) {
+
+	}
+
+	public void slip() {
+
+	}
+
+	public void run() {
+
+	}
+}
