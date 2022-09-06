275b35350f7351a03a8d3f9b9f3823d9836566b7
coralineis <100649638+coralineis@users.noreply.github.com>
2022-09-06 14:15:15 +0900

    chap8 practice8-4

--- SukkiriJavaNyumon/src/game/Cleric.java
+++ SukkiriJavaNyumon/src/game/Cleric.java
@@ -1,5 +1,7 @@
 package game;
 
+import java.util.*;
+
 public class Cleric {
 	String name;
 	int hp = 50;
@@ -12,5 +14,16 @@ public class Cleric {
 		this.hp = this.MAX_HP;
 		this.mp -= 5;
 		System.out.println("HPが最大まで回復した");
+	}
+
+	public int pray(int sec) {
+		System.out.println(this.name + "は" + sec + "は祈った！");
+		int recover = new java.util.Random().nextInt(3) + sec;
+		int recoverActual = Math.min(this.MAX_MP - this.mp, recover);
+
+		this.mp += recoverActual;
+		System.out.println("MPが" + recoverActual + "回復した");
+		return recoverActual;
+
 	}
 }
