699b221bf552ec564aefc4af35af0c95e69a7360
coralineis <100649638+coralineis@users.noreply.github.com>
2022-09-06 14:52:29 +0900

    chap9-1-10

--- SukkiriJavaNyumon/src/game/Main.java
+++ SukkiriJavaNyumon/src/game/Main.java
@@ -6,11 +6,22 @@ public class Main {
 		s.name = "炎の剣";
 		s.damage = 10;
 
-		Hero h = new Hero();
-		h.name = "ミナト";
-		h.hp = 100;
-		h.sword = s;
-		System.out.println("現在の武器は" + h.sword.name);
+		Hero h1 = new Hero();
+		h1.name = "ミナト";
+		h1.hp = 100;
+		h1.sword = s;
+		System.out.println("現在の武器は" + h1.sword.name);
+
+		Hero h2 = new Hero();
+		h2.name = "アサカ";
+		h2.hp = 100;
+
+		Wizard w = new Wizard();
+		w.name = "スガワラ";
+		w.hp = 50;
+		w.heal(h1);
+		w.heal(h2);
+		w.heal(h2);
 
 		Matango m1 = new Matango();
 		m1.hp = 50;
@@ -20,10 +31,10 @@ public class Main {
 		m2.hp = 48;
 		m2.suffix = 'B';
 
-		h.sit(5);
-		h.slip();
-		h.sit(25);
-		h.run();
-		h.attack();
+		h1.sit(5);
+		h1.slip();
+		h1.sit(25);
+		h1.run();
+		h1.attack();
 	}
 }
---
+++ SukkiriJavaNyumon/src/game/Wizard.java
@@ -0,0 +1,11 @@
+package game;
+
+public class Wizard {
+	String name;
+	int hp;
+
+	public void heal(Hero h) {
+		h.hp += 10;
+		System.out.println(h.name + "のHPを10回復した！");
+	}
+}
