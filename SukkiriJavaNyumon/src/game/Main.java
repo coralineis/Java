2d5f8545d0be11426d092f7a32167e4ee14e271c
coralineis <100649638+coralineis@users.noreply.github.com>
2022-09-06 13:56:09 +0900

    sjn 8-4

--- SukkiriJavaNyumon/src/game/Main.java
+++ SukkiriJavaNyumon/src/game/Main.java
@@ -2,6 +2,21 @@ package game;
 
 public class Main {
 	public static void main(String[] args) {
+		Hero h = new Hero();
+		h.name = "ミナト";
+		h.hp = 100;
 
+		Matango m1 = new Matango();
+		m1.hp = 50;
+		m1.suffix = 'A';
+
+		Matango m2 = new Matango();
+		m2.hp = 48;
+		m2.suffix = 'B';
+
+		h.sit(5);
+		h.slip();
+		h.sit(25);
+		h.run();
 	}
 }
--- SukkiriJavaNyumon/src/game/Matango.java
+++ SukkiriJavaNyumon/src/game/Matango.java
@@ -3,4 +3,8 @@ package game;
 public class Matango {
 	int hp;
 	final int LEVEL = 10;
+	char suffix;
+	public void run() {
+		System.out.println("お化けキノコ" + this.suffix + "は逃げ出した！");
+	}
 }
