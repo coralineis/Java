d41a641aaca7025fef6f0fbd846441f14d806468
coralineis <100649638+coralineis@users.noreply.github.com>
2022-09-06 14:04:34 +0900

    chap8 practice8-3

--- SukkiriJavaNyumon/src/game/Cleric.java
+++ SukkiriJavaNyumon/src/game/Cleric.java
@@ -6,4 +6,11 @@ public class Cleric {
 	final int MAX_HP = 50;
 	int mp = 10;
 	final int MAX_MP = 10;
+
+	public void selfAid() {
+		System.out.println(this.name + "はセルフエイドを唱えた！");
+		this.hp = this.MAX_HP;
+		this.mp -= 5;
+		System.out.println("HPが最大まで回復した");
+	}
 }
