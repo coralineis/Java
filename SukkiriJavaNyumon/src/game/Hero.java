580a8ba44c61d4b35839de50edad742e863745a5
coralineis <100649638+coralineis@users.noreply.github.com>
2022-09-06 17:40:45 +0900

    chap9-2

--- SukkiriJavaNyumon/src/game/Hero.java
+++ SukkiriJavaNyumon/src/game/Hero.java
@@ -5,6 +5,15 @@ public class Hero {
 	int hp;
 	Sword sword;
 
+	public Hero(String name) {
+		this.hp = 100;
+		this.name = name;
+	}
+
+	public Hero() {
+		this("ダミー");
+	}
+
 	public void attack() {
 		System.out.println(this.name + "は" + this.sword.name + "で攻撃した！");
 		System.out.println("敵に5ポイントのダメージを与えた！");
--- SukkiriJavaNyumon/src/game/Main.java
+++ SukkiriJavaNyumon/src/game/Main.java
@@ -6,22 +6,15 @@ public class Main {
 		s.name = "炎の剣";
 		s.damage = 10;
 
-		Hero h1 = new Hero();
-		h1.name = "ミナト";
-		h1.hp = 100;
-		h1.sword = s;
-		System.out.println("現在の武器は" + h1.sword.name);
+		Hero h1 = new Hero("ミナト");
+		System.out.println(h1.name);
 
 		Hero h2 = new Hero();
-		h2.name = "アサカ";
-		h2.hp = 100;
+		System.out.println(h2.name);
 
 		Wizard w = new Wizard();
 		w.name = "スガワラ";
 		w.hp = 50;
-		w.heal(h1);
-		w.heal(h2);
-		w.heal(h2);
 
 		Matango m1 = new Matango();
 		m1.hp = 50;
