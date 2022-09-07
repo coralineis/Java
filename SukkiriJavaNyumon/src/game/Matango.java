618d5a6513f487d403561e48795507e0ee085b45
coralineis <100649638+coralineis@users.noreply.github.com>
2022-09-07 09:30:42 +0900

    chap10 practice10-3

--- SukkiriJavaNyumon/src/game/Matango.java
+++ SukkiriJavaNyumon/src/game/Matango.java
@@ -1,10 +1,16 @@
 package game;
 
 public class Matango {
-	int hp;
-	final int LEVEL = 10;
+	int hp = 50;
 	char suffix;
-	public void run() {
-		System.out.println("お化けキノコ" + this.suffix + "は逃げ出した！");
+
+	public Matango(char suffix) {
+		this.suffix = suffix;
+	}
+
+	public void attack(Hero h) {
+		System.out.println("キノコ" + this.suffix + "の攻撃");
+		System.out.println("10のダメージ");
+		h.hp -= 10;
 	}
 }
