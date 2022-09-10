bde7aa7fb6ce3af9b40d315ddf6efcc26be7186f
coralineis <100649638+coralineis@users.noreply.github.com>
2022-09-10 13:38:26 +0900

    chap13-1

--- SukkiriJavaNyumon/src/game/Hero.java
+++ SukkiriJavaNyumon/src/game/Hero.java
@@ -3,10 +3,29 @@ package game;
 public class Hero extends Character{
 	String name = "ミナト";
 	int hp = 100;
+	Sword sword;
+
+	public void bye() {
+		System.out.println("勇者は別れを告げた");
+	}
+
+	public void die() {
+		System.out.println(this.name + "は死んでしまった");
+		System.out.println("GAME OVERです");
+	}
+
+	public void sleep() {
+		this.hp = 100;
+		System.out.println(this.name + "は眠って回復した");
+	}
 
 	public void attack(Monster m) {
 		System.out.println(this.name + "の攻撃！");
 		System.out.println("5ポイントのダメージを与えた！");
+		this.hp -= 2;
+		if ( this.hp <= 0) {
+			this.die();
+		}
 	}
 
 	public final void slip() {
