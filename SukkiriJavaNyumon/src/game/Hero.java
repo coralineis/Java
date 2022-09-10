4a2927a321d47eeb54a480f35342625c576bc169
coralineis <100649638+coralineis@users.noreply.github.com>
2022-09-10 14:13:46 +0900

    chap13 practice13-3

--- SukkiriJavaNyumon/src/game/Hero.java
+++ SukkiriJavaNyumon/src/game/Hero.java
@@ -54,4 +54,13 @@ public class Hero extends Character{
 	public void run() {
 		System.out.println(this.name + "は逃げ出した！");
 	}
+
+	public int getHp() {
+		return hp;
+	}
+
+	public void setHp(int hp) {
+		this.hp = hp;
+	}
+
 }
--- SukkiriJavaNyumon/src/game/Wizard.java
+++ SukkiriJavaNyumon/src/game/Wizard.java
@@ -8,8 +8,40 @@ public class Wizard{
 
 	public void heal(Hero h) {
 		int basePoint = 10;
-		int recovPoint = (int)(basePoint * this.wand.power);
+		int recovPoint = (int)(basePoint * this.getWand().getPower());
 		h.setHp(h.getHp() + recovPoint);
-		System.out.println(h.getName + "のHPを" + recovPoint +"回復した！");
+		System.out.println(h.getName() + "のHPを" + recovPoint +"回復した！");
+	}
+
+	public int getHp() {
+		return hp;
+	}
+
+	public void setHp(int hp) {
+		this.hp = hp;
+	}
+
+	public int getMp() {
+		return mp;
+	}
+
+	public void setMp(int mp) {
+		this.mp = mp;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Wand getWand() {
+		return wand;
+	}
+
+	public void setWand(Wand wand) {
+		this.wand = wand;
 	}
 }
