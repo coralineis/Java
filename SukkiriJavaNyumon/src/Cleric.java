852bd178853478386981097643c59aaa98f37afe
coralineis <100649638+coralineis@users.noreply.github.com>
2022-09-13 22:01:38 +0900

    chap14 practice14-2

--- SukkiriJavaNyumon/src/Cleric.java
+++ SukkiriJavaNyumon/src/Cleric.java
@@ -3,9 +3,9 @@ import java.util.*;
 public class Cleric {
 	String name;
 	int hp = 50;
-	final int MAX_HP = 50;
+	static final int MAX_HP = 50;
 	int mp = 10;
-	final int MAX_MP = 10;
+	static final int MAX_MP = 10;
 
 	public void selfAid() {
 		System.out.println(this.name + "はセルフエイドを唱えた！");
