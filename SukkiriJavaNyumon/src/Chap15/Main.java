ba001d17da59d48c6df2a24dfe71a21ed2abe287
coralineis <100649638+coralineis@users.noreply.github.com>
2022-09-18 12:40:44 +0900

    chap15-6

--- SukkiriJavaNyumon/src/Chap15/Main.java
+++ SukkiriJavaNyumon/src/Chap15/Main.java
@@ -1,9 +1,16 @@
 package Chap15;
 
+import java.util.Date;
+
 public class Main {
 	public static void main(String[] args) {
-		String s = "abc,def:ghi";
-		String w = s.replaceAll("[beh]", "X");
-		System.out.println(w);
+		Date now = new Date();
+		System.out.println(now);
+		System.out.println(now.getTime());
+		Date past = new Date(1600705425827L);
+		System.out.println(past);
+		long start = System.currentTimeMillis();
+		long end = System.currentTimeMillis();
+		System.out.println("処理にかかった時間は…" + (end - start) + "ミリ秒でした");
 	}
 }
