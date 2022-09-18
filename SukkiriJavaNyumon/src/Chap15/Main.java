9f7d1cdc5078b0219f82499e69749f3811a99ed4
coralineis <100649638+coralineis@users.noreply.github.com>
2022-09-18 12:11:36 +0900

    chap15-4

--- SukkiriJavaNyumon/src/Chap15/Main.java
+++ SukkiriJavaNyumon/src/Chap15/Main.java
@@ -3,9 +3,7 @@ package Chap15;
 public class Main {
 	public static void main(String[] args) {
 		String s = "abc,def:ghi";
-		String[] words = s.split("[,:]");
-		for (String w : words) {
-			System.out.print(w + "->");
-		}
+		String w = s.replaceAll("[beh]", "X");
+		System.out.println(w);
 	}
 }
