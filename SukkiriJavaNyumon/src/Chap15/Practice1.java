fbcc516fcef9552a915a29620b29a8c8b3c105ba
coralineis <100649638+coralineis@users.noreply.github.com>
2022-10-04 20:23:58 +0900

    chap15 practice15-1

---
+++ SukkiriJavaNyumon/src/Chap15/Practice1.java
@@ -0,0 +1,12 @@
+package Chap15;
+
+public class Practice1 {
+	public static void main(String[] args) {
+		StringBuilder sb = new StringBuilder();
+		for (int i = 0; i < 100; i++) {
+			sb.append(i+1).append(",");
+		}
+		String s = sb.toString();
+		String[] a = s.split(",");
+	}
+}
