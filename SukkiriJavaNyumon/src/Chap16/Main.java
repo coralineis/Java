f691f477d75b6d291d22e5df60ca7c31131837c4
coralineis <100649638+coralineis@users.noreply.github.com>
2022-10-06 20:30:33 +0900

    chap16-2

--- SukkiriJavaNyumon/src/Chap16/Main.java
+++ SukkiriJavaNyumon/src/Chap16/Main.java
@@ -1,15 +1,17 @@
 package Chap16;
 
-import java.util.ArrayList;
+import java.util.*;
 
 public class Main {
 	public static void main(String[] args) {
-		ArrayList<Integer> points = new ArrayList<Integer>();
-		points.add(10);
-		points.add(80);
-		points.add(75);
-		for (int i : points) {
-			System.out.println(i);
+		ArrayList<String> names = new ArrayList<String>();
+		names.add("湊");
+		names.add("朝香");
+		names.add("菅原");
+		Iterator<String> it = names.iterator();
+		while (it.hasNext()) {
+			String e = it.next();
+			System.out.println(e);
 		}
 	}
 }
