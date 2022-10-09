f8996f699ec8ee1acc3d270f5daf8225896bd796
coralineis <100649638+coralineis@users.noreply.github.com>
2022-10-09 10:34:33 +0900

    chap16-5 collection Map

--- SukkiriJavaNyumon/src/Chap16/Main.java
+++ SukkiriJavaNyumon/src/Chap16/Main.java
@@ -1,17 +1,18 @@
 package Chap16;
 
-import java.util.Set;
-import java.util.TreeSet;
+import java.util.*;
 
 public class Main {
 	public static void main(String[] args) {
-		Set<String> words = new TreeSet<String>();
-		words.add("dog");
-		words.add("cat");
-		words.add("wolf");
-		words.add("panda");
-		for (String s : words) {
-			System.out.print(s + "→");
-		}
+		Map<String, Integer> prefs = new HashMap<String, Integer>();
+		prefs.put("京都府", 255);
+		prefs.put("東京都", 1261);
+		prefs.put("熊本県", 181);
+		int tokyo = prefs.get("東京都");
+		System.out.println("東京都の人口は、" + tokyo);
+		prefs.remove("京都府");
+		prefs.put("熊本県", 182);
+		int kumamoto = prefs.get("熊本県");
+		System.out.println("熊本県の人口は、" + kumamoto);
 	}
 }
