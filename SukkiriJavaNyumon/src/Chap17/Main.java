1ae57ee500205549989c1a4eed8fa243f25c9a8f
coralineis <100649638+coralineis@users.noreply.github.com>
2022-10-09 12:14:41 +0900

    chap17-3 exception

---
+++ SukkiriJavaNyumon/src/Chap17/Main.java
@@ -0,0 +1,13 @@
+package Chap17;
+
+import java.io.*;
+
+public class Main {
+	public static void main(String[] args) {
+		try {
+			FileWriter fw = new FileWriter("data.txt");
+		} catch (IOException e) {
+			System.out.println("エラーが発生しました。");
+		}
+	}
+}
