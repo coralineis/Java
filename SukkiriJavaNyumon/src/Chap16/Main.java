0f8f509a1dbf7720ec529c53b42a018c37fe57db
coralineis <100649638+coralineis@users.noreply.github.com>
2022-10-09 11:11:24 +0900

    chap16-5 Map for

--- SukkiriJavaNyumon/src/Chap16/Main.java
+++ SukkiriJavaNyumon/src/Chap16/Main.java
@@ -7,12 +7,10 @@ public class Main {
 		Map<String, Integer> prefs = new HashMap<String, Integer>();
 		prefs.put("京都府", 255);
 		prefs.put("東京都", 1261);
-		prefs.put("熊本県", 181);
-		int tokyo = prefs.get("東京都");
-		System.out.println("東京都の人口は、" + tokyo);
-		prefs.remove("京都府");
 		prefs.put("熊本県", 182);
-		int kumamoto = prefs.get("熊本県");
-		System.out.println("熊本県の人口は、" + kumamoto);
+		for (String key : prefs.keySet()) {
+			int value = prefs.get(key);
+			System.out.println(key + "の人口は、" + value);
+		}
 	}
 }
